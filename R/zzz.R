48fec59e8728b9e87c6227114f14b052754d688f
Pascal Burkhard <pascal.burkhard@gmail.com>
2021-11-08 07:54:07 +0100

    Move font installation to geovizr and use showtext

--- R/zzz.R
+++ R/zzz.R
@@ -9,3 +9,20 @@ magrittr::`%<>%`
 #' @importFrom rlang !!
 #' @export
 rlang::`!!`
+
+.onLoad = function(lib, pkg) {
+  # Use showtext ------------------------------------------------------------
+  showtext::showtext_auto()
+
+
+  # Check registered fonts --------------------------------------------------
+  registered <- sysfonts::font_families()
+  needed <- c("Helvetica", "Fira Sans", "Fira Sans Light")
+
+  check_registered_fonts <- function(font) {
+    if (!(font %in% registered)) geovizr::gvz_register_fonts()
+  }
+
+  needed |>
+    purrr::walk(check_registered_fonts)
+}
