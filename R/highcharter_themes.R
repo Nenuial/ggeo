818fe798856bcefd6bd210c7266a8c6715066613
Pascal Burkhard <pascal.burkhard@gmail.com>
2025-02-22 11:03:43 +0100

    refactor: hc themes refactor

--- R/highcharter_themes.R
+++ R/highcharter_themes.R
@@ -25,7 +25,6 @@
 #'   highcharter::hcaes(x = speed, y = dist)
 #' ) |>
 #'   hc_web_theme()
-#'
 hc_purple_theme <- function(hc) {
   base_colors <- list(
     main_color  = "#e0d9fb",
@@ -33,40 +32,82 @@ hc_purple_theme <- function(hc) {
     title_color = "#a84890"
   )
 
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    wesanderson::wes_palette("GrandBudapest2")
+  )
+
+  hc |> highcharter::hc_add_theme(theme)
+}
+
+#' Function factory to create a highcharter theme
+#'
+#' @param base_colors A list with the main, axis and title colors
+#' @param palette A color palette
+#' @param web_theme A boolean to indicate if the theme is for the web
+#' @param title_font_size The title font size
+#' @param title_font_weight The title font weight
+#' @param subtitle_font_size The subtitle font size
+#' @param subtitle_font_weight The subtitle font weight
+#'
+#' @returns A highcharter theme object
+#' @keywords internal
+ggeo_create_highchart_theme <- function(base_colors, palette, web_theme = FALSE,
+                                        title_font_size = if (!web_theme) "52px" else NULL,
+                                        title_font_weight = if (!web_theme) "400" else NULL,
+                                        subtitle_font_size = if (!web_theme) "38px" else NULL,
+                                        subtitle_font_weight = if (!web_theme) "300" else NULL) {
   base_text_style <- list(
     fontFamily = "Fira Sans",
-    fontWeight = "100",
-    fontSize = "24px",
     color = base_colors$main_color
   )
+  if (!web_theme) {
+    base_text_style$fontWeight <- "100"
+    base_text_style$fontSize <- "24px"
+  }
 
   axis_title_style <- list(
     fontFamily = "Fira Sans",
-    fontWeight = "300",
-    fontSize = "24px",
     color = base_colors$axis_color
   )
+  if (!web_theme) {
+    axis_title_style$fontWeight <- "300"
+    axis_title_style$fontSize <- "24px"
+  }
 
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
+  title_style <- list(
+    color = base_colors$title_color,
+    fontFamily = "Fira Sans"
+  )
+  if (!is.null(title_font_size)) title_style$fontSize <- title_font_size
+  if (!is.null(title_font_weight)) title_style$fontWeight <- title_font_weight
+
+  subtitle_style <- list(
+    color = base_colors$title_color,
+    fontFamily = "Fira Sans"
+  )
+  if (!is.null(subtitle_font_size)) subtitle_style$fontSize <- subtitle_font_size
+  if (!is.null(subtitle_font_weight)) subtitle_style$fontWeight <- subtitle_font_weight
+
+  tooltip_style <- list(fontFamily = "Fira Sans")
+  if (!web_theme) {
+    tooltip_style$fontWeight <- "200"
+    tooltip_style$fontSize <- "28px"
+    tooltip_header <- "<span style='font-size: 24px'>{point.key}</span><br/>"
+  } else {
+    tooltip_header <- NULL
+  }
+
+  highcharter::hc_theme(
+    colors = palette,
     chart = list(
       backgroundColor = "transparent"
     ),
     title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "52px",
-        fontWeight = "400"
-      )
+      style = title_style
     ),
     subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "38px",
-        fontWeight = "300"
-      )
+      style = subtitle_style
     ),
     legend = list(
       itemStyle = base_text_style,
@@ -74,14 +115,11 @@ hc_purple_theme <- function(hc) {
         color = base_colors$main_color
       )
     ),
-    tooltip = list(
-      headerFormat = "<span style='font-size: 24px'>{point.key}</span><br/>",
-      style = list(
-        fontFamily = "Fira Sans",
-        fontWeight = "200",
-        fontSize = "28px"
-      )
-    ),
+    tooltip = if (!is.null(tooltip_header)) {
+      list(headerFormat = tooltip_header, style = tooltip_style)
+    } else {
+      list(style = tooltip_style)
+    },
     caption = list(
       style = base_text_style
     ),
@@ -102,9 +140,6 @@ hc_purple_theme <- function(hc) {
       )
     )
   )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
 }
 
 #' @rdname hc_purple_theme
@@ -116,78 +151,12 @@ hc_samarqand_theme <- function(hc) {
     title_color = "#6098c0"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "100",
-    fontSize = "24px",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    wesanderson::wes_palette("GrandBudapest2")
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "300",
-    fontSize = "24px",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "52px",
-        fontWeight = "400"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "38px",
-        fontWeight = "300"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      headerFormat = "<span style='font-size: 24px'>{point.key}</span><br/>",
-      style = list(
-        fontFamily = "Fira Sans",
-        fontWeight = "200",
-        fontSize = "28px"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
 
 #' @rdname hc_purple_theme
@@ -199,78 +168,12 @@ hc_pomogreen_theme <- function(hc) {
     title_color = "#919c4c"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "100",
-    fontSize = "24px",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    wesanderson::wes_palette("GrandBudapest2")
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "300",
-    fontSize = "24px",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "52px",
-        fontWeight = "400"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "38px",
-        fontWeight = "300"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      headerFormat = "<span style='font-size: 24px'>{point.key}</span><br/>",
-      style = list(
-        fontFamily = "Fira Sans",
-        fontWeight = "200",
-        fontSize = "28px"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
 
 #' @rdname hc_purple_theme
@@ -282,80 +185,12 @@ hc_jerusalem_theme <- function(hc) {
     title_color = "#699CB6"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "100",
-    fontSize = "24px",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors, wesanderson::wes_palette("GrandBudapest2")
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "300",
-    fontSize = "24px",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "52px",
-        fontWeight = "400"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "38px",
-        fontWeight = "300"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      headerFormat = "<span style='font-size: 24px'>{point.key}</span><br/>",
-      style = list(
-        fontFamily = "Fira Sans",
-        fontWeight = "200",
-        fontSize = "28px"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
-
 
 #' @rdname hc_purple_theme
 #' @export
@@ -366,78 +201,12 @@ hc_guonian_theme <- function(hc) {
     title_color = "#be3b49"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "100",
-    fontSize = "24px",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    PNWColors::pnw_palette("Bay", 10)
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    fontWeight = "300",
-    fontSize = "24px",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = PNWColors::pnw_palette("Bay", 10),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "52px",
-        fontWeight = "400"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans",
-        fontSize = "38px",
-        fontWeight = "300"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      headerFormat = "<span style='font-size: 24px'>{point.key}</span><br/>",
-      style = list(
-        fontFamily = "Fira Sans",
-        fontWeight = "200",
-        fontSize = "28px"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
 
 #' @rdname hc_purple_theme
@@ -449,67 +218,13 @@ hc_web_theme <- function(hc) {
     title_color = "#73819B"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    wesanderson::wes_palette("GrandBudapest2"),
+    web_theme = TRUE
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      style = list(
-        fontFamily = "Fira Sans"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
 
 #' @rdname hc_purple_theme
@@ -521,65 +236,11 @@ hc_dark_web_theme <- function(hc) {
     title_color = "#ffffff"
   )
 
-  base_text_style <- list(
-    fontFamily = "Fira Sans",
-    color = base_colors$main_color
+  theme <- ggeo_create_highchart_theme(
+    base_colors,
+    wesanderson::wes_palette("GrandBudapest2"),
+    web_theme = TRUE
   )
 
-  axis_title_style <- list(
-    fontFamily = "Fira Sans",
-    color = base_colors$axis_color
-  )
-
-  theme <- highcharter::hc_theme(
-    colors = wesanderson::wes_palette("GrandBudapest2"),
-    chart = list(
-      backgroundColor = "transparent"
-    ),
-    title = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans"
-      )
-    ),
-    subtitle = list(
-      style = list(
-        color = base_colors$title_color,
-        fontFamily = "Fira Sans"
-      )
-    ),
-    legend = list(
-      itemStyle = base_text_style,
-      itemHoverStyle = list(
-        color = base_colors$main_color
-      )
-    ),
-    tooltip = list(
-      style = list(
-        fontFamily = "Fira Sans"
-      )
-    ),
-    caption = list(
-      style = base_text_style
-    ),
-    xAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    ),
-    yAxis = list(
-      labels = list(
-        style = base_text_style
-      ),
-      title = list(
-        style = axis_title_style
-      )
-    )
-  )
-
-  hc |>
-    highcharter::hc_add_theme(theme)
+  hc |> highcharter::hc_add_theme(theme)
 }
